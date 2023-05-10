a125ebee8e8dc10af3216550165a5a3c8e1bbdfb
root <tayler.coon@gmail.com>
2023-05-10 10:14:33 -0700

    task 9 functional

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-
+def print_last_digit(number):
+    if number < 0:
+        alt_num = number * -1
+        last = (alt_num % 10)
+        if last >= 0:
+            print("{}".format(last), end="")
+            return last
+        else:
+            last = last * -1
+            print("{}".format(last), end="")
+            return last
+    else:
+        last = (number % 10)
+        print("{}".format(last), end="")
+        return last
