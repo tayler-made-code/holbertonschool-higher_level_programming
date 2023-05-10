7b9b9b0ceaa1d3c0ed3dd0515399428089d3c8ce
root <tayler.coon@gmail.com>
2023-05-10 07:05:21 -0700

    fixed lines too long

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,16 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     convert = number * -1
-    lastdig = (convert % 10)
-    if lastdig == 0:
-        print(f"Last digit of {number} is {lastdig} and is 0")
+    last = (convert % 10)
+    if last == 0:
+        print(f"Last digit of {number} is {last} and is 0")
     else:
-        print(f"Last digit of {number} is {lastdig * -1} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {last * -1} and is less than 6 and not 0")
 else:
-    lastdig = (number % 10)
-    if lastdig > 5:
-        print(f"Last digit of {number} is {lastdig} and is greater than 5")
-    elif lastdig == 0:
-        print(f"Last digit of {number} is {lastdig} and is 0")
+    last = (number % 10)
+    if last > 5:
+        print(f"Last digit of {number} is {last} and is greater than 5")
+    elif last == 0:
+        print(f"Last digit of {number} is {last} and is 0")
     else:
-        print(f"Last digit of {number} is {lastdig} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {last} and is less than 6 and not 0")
