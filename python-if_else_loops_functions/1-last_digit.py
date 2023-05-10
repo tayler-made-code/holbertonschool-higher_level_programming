5e46cddfb45d0e290c90c5fe2cd8faaf288e02dc
root <tayler.coon@gmail.com>
2023-05-10 07:01:26 -0700

    fixed negative interactions

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 5:
-    print(f"Last digit of {number} is {number % 10} and is greater than 5")
-elif number == 0:
-    print(f"Last digit of {number} is {number % 10} and is 0")
+if number < 0:
+    convert = number * -1
+    lastdig = (convert % 10)
+    if lastdig == 0:
+        print(f"Last digit of {number} is {lastdig} and is 0")
+    else:
+        print(f"Last digit of {number} is {lastdig * -1} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
+    lastdig = (number % 10)
+    if lastdig > 5:
+        print(f"Last digit of {number} is {lastdig} and is greater than 5")
+    elif lastdig == 0:
+        print(f"Last digit of {number} is {lastdig} and is 0")
+    else:
+        print(f"Last digit of {number} is {lastdig} and is less than 6 and not 0")
