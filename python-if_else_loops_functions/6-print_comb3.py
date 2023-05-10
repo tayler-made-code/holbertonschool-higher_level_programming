967c992b28a8366a0adffb915731a1e9ca3002c7
root <tayler.coon@gmail.com>
2023-05-10 08:58:41 -0700

    updated for P8 compliance

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range (0, 10):
-    for j in range (i + 1, 10):
+for i in range(0, 10):
+    for j in range(i + 1, 10):
         if (i != 8) and (i != 9):
             print("{:d}{:d}, ".format(i, j), end="")
         else:
