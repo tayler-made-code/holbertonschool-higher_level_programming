ed6323fb9a8c119cd3d2624fa734e4c3f0351fe9
root <tayler.coon@gmail.com>
2023-05-10 19:26:18 -0700

    fixed P8 styling

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     sum = 0
-    for i in range (1, len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         sum += int(sys.argv[i])
     print(sum)
