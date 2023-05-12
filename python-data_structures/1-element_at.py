0101ed3a9ae8d230c168083859f10e13ed4af5ef
root <tayler.coon@gmail.com>
2023-05-12 10:34:09 -0700

    PEP8 Validation

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     listlen = len(my_list)
-    if listlen < idx or idx < 0:
+    if idx > listlen or idx < 0:
         return None
     else:
         return my_list[idx]
-
--- python-data_structures/1-main.py
+++ python-data_structures/1-main.py
@@ -3,4 +3,8 @@ element_at = __import__('1-element_at').element_at
 
 my_list = [1, 2, 3, 4, 5]
 idx = 3
+idx2 = -1
+idx3 = 9
 print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+print("Element at index {:d} is {}".format(idx2, element_at(my_list, idx2)))
+print("Element at index {:d} is {}".format(idx3, element_at(my_list, idx3)))
