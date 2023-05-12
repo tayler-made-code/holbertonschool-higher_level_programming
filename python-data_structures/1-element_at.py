94fcf06df98b75c7843e0ccb29369a9722cd6e27
root <tayler.coon@gmail.com>
2023-05-12 10:09:00 -0700

    fixed negative interaction

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    #if idx  is negative return None
-    #if idx is greater than my_list return None
     listlen = len(my_list)
-    if listlen < idx or listlen < 0:
+    if listlen < idx or idx < 0:
         return None
     else:
         return my_list[idx]
