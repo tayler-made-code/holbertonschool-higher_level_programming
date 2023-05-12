292c671aca4c80476731f5f76df65a49a19353b5
root <tayler.coon@gmail.com>
2023-05-12 10:59:43 -0700

    task 2 functional

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    listlen = len(my_list)
-    if idx > listlen or idx < 0:
+    if idx > len(my_list) or idx < 0:
         return None
-    else:
-        return my_list[idx]
+    return my_list[idx]
