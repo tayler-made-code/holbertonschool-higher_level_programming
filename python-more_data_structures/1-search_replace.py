f12c81abc3e8ce599234e3785cd6fff2f0531cb4
root <tayler.coon@gmail.com>
2023-05-14 14:11:24 -0700

    task 1 functional

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    santas_list = my_list[:]
+    for i in range(len(santas_list)):
+        if santas_list[i] == search:
+            santas_list[i] = replace
+    return santas_list
 
