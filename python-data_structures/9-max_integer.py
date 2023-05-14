c62f55ae8b157a2866db9e35faabecbbd43ae361
root <tayler.coon@gmail.com>
2023-05-14 08:53:08 -0700

    fixed pycodestyling

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-   if (len(my_list)) == 0:
-       return None
-   elif (len(my_list)) == 1:
-       return (my_list[0])
-   else:
-       my_list.sort()
-       return (my_list[len(my_list) - 1])
+    if (len(my_list)) == 0:
+        return None
+    elif (len(my_list)) == 1:
+        return (my_list[0])
+    else:
+        my_list.sort()
+        return (my_list[len(my_list) - 1])
