f77bc76b128acfe302936cd7067829856284d2bb
root <tayler.coon@gmail.com>
2023-05-14 13:56:47 -0700

    working on local machine

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     matrix_reloaded = []
     for i in matrix:
-        matrix_reloaded.append(list(map(lambda x: x**2, i)))
-    return matrix_reloaded
+         matrix_reloaded.append(list(map(lambda x: x**2, i)))
+    return (matrix_reloaded)
