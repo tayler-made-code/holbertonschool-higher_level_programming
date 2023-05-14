42af9e55946baf44a2408427cbe3cbb5f8dfdaef
root <tayler.coon@gmail.com>
2023-05-14 09:03:30 -0700

    task 10 functional

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def divisible_by_2(my_list=[]):
+    new_list = my_list[:]
+    for i in new_list:
+        if (new_list[i] % 2) == 1:
+            new_list[i] = False
+        else:
+            new_list[i] = True
+    return new_list
