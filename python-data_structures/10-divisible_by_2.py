f1c04e3389ba93bc5b40e97399c8f8a30e4af277
root <tayler.coon@gmail.com>
2023-05-14 10:31:48 -0700

    guess it wasn't, task 10 is now fully functional

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = my_list[:]
+    j = 0
     for i in new_list:
         if i % 2 == 1:
-            new_list[i] = False
+            new_list[j] = False
+            j += 1
         else:
-            new_list[i] = True
+            new_list[j] = True
+            j += 1
     return new_list
