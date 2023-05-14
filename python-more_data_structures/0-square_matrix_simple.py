5705ba7f07ab05ee5e4b5ca39031347b48b666aa
root <tayler.coon@gmail.com>
2023-05-14 13:25:55 -0700

    task 0 functional

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-
+def square_matrix_simple(matrix=[]):
+    matrix_reloaded = []
+    for i in matrix:
+        matrix_reloaded.append(list(map(lambda x: x**2, i)))
+    return matrix_reloaded
