e4599e9dbc602b453cf8ec7f7512fc302092c809
root <tayler.coon@gmail.com>
2023-05-14 16:18:12 -0700

    task 9 functional

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-
+def multiply_by_2(a_dictionary):
+    seeing_double = {}
+    for key, value in a_dictionary.items():
+        seeing_double[key] = value * 2
+    return seeing_double
