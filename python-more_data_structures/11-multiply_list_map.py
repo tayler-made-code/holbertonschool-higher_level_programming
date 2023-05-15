7f710c8525c4fc8380424ca02a26a2c9cb9e2453
root <tayler.coon@gmail.com>
2023-05-15 12:40:03 -0700

    task 11 functional

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    late_fees = my_list[:]
-    for i in range(len(late_fees)):
-        late_fees[i] *= number
-    return late_fees
+    return list(map(lambda x: x * number, my_list))
