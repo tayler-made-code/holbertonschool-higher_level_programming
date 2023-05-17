fe5dff17af3b457041bc38c1b156ffd135360f85
root <tayler.coon@gmail.com>
2023-05-16 21:09:53 -0700

    task 12 mostly working

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == None:
+    if roman_string is None:
         return 0
     roman_numeral = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    total = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman_numeral[roman_string[i]] > roman_numeral[roman_string[i - 1]]:
+            total += roman_numeral[roman_string[i]] - 2 * roman_numeral[roman_numeral[i - 1]]
+        else:
+            total += roman_numeral[roman_string[i]]
+    return total
