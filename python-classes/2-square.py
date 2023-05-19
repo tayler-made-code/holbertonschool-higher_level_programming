64e809ef83d0699f6df57ec4c2ef3a3e789b5fc1
root <tayler.coon@gmail.com>
2023-05-19 14:38:29 -0700

    task 2 functional

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""This module creates a class Square
+"""
 
+
+class Square:
+    """This class is an empty class
+    """
+    def __init__(self, size=0):
+        """This function creates a private instance size
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
