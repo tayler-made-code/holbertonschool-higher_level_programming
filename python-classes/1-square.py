8d3f1d647eee38a72407b9273057fab0c5d74231
root <tayler.coon@gmail.com>
2023-05-19 14:30:21 -0700

    task 1 functional

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""This module creates a class Square
+"""
 
+
+class Square:
+    """This class is an empty class
+    """
+    def __init__(self, size):
+        """This function creates a private instance size
+        """
+        self.__size = size
