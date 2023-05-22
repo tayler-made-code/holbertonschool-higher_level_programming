2fefdce8e5c9a6191a2b2dd83535b499f359566d
root <tayler.coon@gmail.com>
2023-05-22 10:17:59 -0700

    task 5 functional

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,2 +1,45 @@
 #!/usr/bin/python3
+"""This module creates a class Square
+"""
 
+
+class Square:
+    """This class creates a private instance attribute size
+    """
+    def __init__(self, size=0):
+        """This function creates a private instance size
+        """
+        self.__size = size
+
+    def area(self):
+        """This function returns the current square area
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """This function returns the size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """This function sets the size
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """This function prints the square in stdout with # characters
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
