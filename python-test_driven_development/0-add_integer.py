4e8c4407624ee1ad89efa08c680d3f806d7afe48
root <tayler.coon@gmail.com>
2023-05-22 14:31:17 -0700

    task 0 functional

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+""" function that adds 2 integers """
 
+
+def add_integer(a, b=98):
+    """ function that adds 2 integers """
+    if type (a) is not int and type (a) is not float:
+        raise TypeError ("a must be an integer")
+    if type (b) is not int and type (b) is not float:
+        raise TypeError ("b must be an integer")
+    else:
+        return int(a) + int(b)
