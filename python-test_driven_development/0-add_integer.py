7caf70e87490d2f09981244dcb5860bbf44e5e67
root <tayler.coon@gmail.com>
2023-05-22 14:33:10 -0700

    updated syntax with pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,9 +4,9 @@
 
 def add_integer(a, b=98):
     """ function that adds 2 integers """
-    if type (a) is not int and type (a) is not float:
-        raise TypeError ("a must be an integer")
-    if type (b) is not int and type (b) is not float:
-        raise TypeError ("b must be an integer")
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
     else:
         return int(a) + int(b)
