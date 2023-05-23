a6e996b47df84c62d23087335788d4ada1742ae8
root <tayler.coon@gmail.com>
2023-05-23 08:15:22 -0700

    say may name functional with test file

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+""" function that prints My name is <first name> <last name> """
 
+
+ErrMsgFirst = "first_name must be a string"
+ErrMsgLast = "last_name must be a string"
+
+
+def say_my_name(first_name, last_name=""):
+    """ function that prints My name is <first name> <last name> """
+    if type(first_name) is not str:
+        raise TypeError(ErrMsgFirst)
+    if type(last_name) is not str:
+        raise TypeError(ErrMsgLast)
+    else:
+        print("My name is {} {}".format(first_name, last_name))
