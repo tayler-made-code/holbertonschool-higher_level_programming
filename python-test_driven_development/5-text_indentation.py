11c34bc2dae05019b906f06cfd3779186a31f88f
root <tayler.coon@gmail.com>
2023-05-23 09:23:52 -0700

    text indentation functional

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+""" fucntion that prints a text with 2 new lines after/
+    each of these characters: ., ?, and : """
 
+
+ErrMsg = "text must be a string"
+
+
+def text_indentation(text):
+    """ function that prints a text with 2 new lines after/
+        each of these characters: ., ?, and : """
+    if type(text) is not str:
+        raise TypeError(ErrMsg)
+    for i in range(len(text)):
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print(text[i])
+            print()
+        else:
+            print(text[i], end="")
