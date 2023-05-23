70e8fb95bfaae4e33c16596e068b55394e4d6155
root <tayler.coon@gmail.com>
2023-05-23 13:48:07 -0700

    now it's working

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,6 +4,42 @@
 
 
 class Rectangle:
-    """ empty class Rectangle that defines a rectangle
+    """ class Rectangle that defines a rectangle
     """
-    pass
+    def __init__(self, width=0, height=0):
+        """ initializes a rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ gets width of rectangle
+        """
+        return self.__width
+    
+    @property
+    def height(self):
+        """ gets height of rectangle
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """ sets width of rectangle
+        """
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise TypeError('width must be >= 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ sets height of rectangle
+        """
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise TypeError('height must be >= 0')
+        self.__height = value
