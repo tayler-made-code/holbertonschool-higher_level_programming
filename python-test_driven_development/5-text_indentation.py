f60694bc69a2c0ac1e11e7d86b5e8129945cabc1
root <tayler.coon@gmail.com>
2023-05-23 10:43:47 -0700

    updated for edge case with multiple spaces

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -18,7 +18,7 @@ def text_indentation(text):
             print()
             break
         if text[i] == ' ':
-            if text[i - 1] == '.' or text[i - 1] == '?' or text[i - 1] == ':':
+            if text[i - 1] == '.' or text[i - 1] == '?' or text[i - 1] == ':' or text[i - 1] == ' ':
                 continue
         if text[i] == '.' or text[i] == '?' or text[i] == ':':
             print(text[i])
