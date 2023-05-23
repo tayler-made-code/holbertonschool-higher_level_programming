64ae5c2febd6b51694e3c0e6e79bcba8e1fe9746
root <tayler.coon@gmail.com>
2023-05-23 13:12:05 -0700

    added more edge cases

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,3 +10,27 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         test_list = [1, 2, 3, 4]
         self.assertEqual(max_integer(test_list), 4)
+
+    def test_max_at_beginning(self):
+        test_list = [4, 1, 3, 2]
+        self.assertEqual(max_integer(test_list), 4)
+
+    def test_max_in_the_middle(self):
+        test_list = [1, 2, 5, 3, 4]
+        self.assertEqual(max_integer(test_list), 5)
+
+    def test_one_neg_number(self):
+        test_list = [-1, 2, 3, 4]
+        self.assertEqual(max_integer(test_list), 4)
+
+    def test_only_neg_numbers(self):
+        test_list = [-1, -2, -3, -4]
+        self.assertEqual(max_integer(test_list), -1)
+
+    def test_one_element(self):
+        test_list = [1]
+        self.assertEqual(max_integer(test_list), 1)
+
+    def test_empty_list(self):
+        test_list = []
+        self.assertEqual(max_integer(test_list), None)
