31de72e536a643a9982531173651ce6cfe12f265
root <tayler.coon@gmail.com>
2023-05-23 16:53:52 -0700

    added class method

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -100,3 +100,9 @@ class Rectangle:
             return rect_1
         else:
             return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """ returns a new Rectangle instance with width == height == size
+        """
+        return cls(size, size)
