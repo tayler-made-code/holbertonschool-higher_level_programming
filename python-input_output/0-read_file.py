2c636488ca5716fae5f3d0295dc1b7395c99ca94
root <tayler.coon@gmail.com>
2023-05-29 14:49:18 -0700

    task 0 functional

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+''' Function that reads a text file (UTF8) and prints it to stdout '''
 
+
+def read_file(filename=""):
+    ''' Function that reads a text file (UTF8) and prints it to stdout '''
+    with open(filename, encoding='utf-8') as holby:
+        print(holby.read(), end="")
