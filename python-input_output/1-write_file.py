346ca113fc4db20eb84bbed43a7c6efb15384ba1
root <tayler.coon@gmail.com>
2023-05-29 14:53:23 -0700

    task 1 functional

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+''' function that writes a string to a text file (UTF8) and returns
+    the number of characters written '''
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding='utf-8') as holby:
+        return holby.write(text)
