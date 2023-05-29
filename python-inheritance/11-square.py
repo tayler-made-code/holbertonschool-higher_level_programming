e4a2a56b2113b370a28498e0d20e2b490018d3d8
root <tayler.coon@gmail.com>
2023-05-29 14:27:07 -0700

    task 11 functional

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -10,3 +10,7 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.integer_validator('size', size)
         self.__size = size
+
+    def __str__(self):
+        ''' This function returns a string representation of a Square '''
+        return '[Square] {}/{}'.format(self.__size, self.__size)
