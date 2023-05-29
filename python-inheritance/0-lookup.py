03bc07a148c1641a27836c6db313bdd0721cfef4
root <tayler.coon@gmail.com>
2023-05-29 10:26:41 -0700

    task 0 functioning

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
+""" function that returns the list of available attributes and methods of an object """
 
+
+def lookup(obj):
+    """ function that returns the list of available attributes and methods of an object """
+    return dir(obj)
