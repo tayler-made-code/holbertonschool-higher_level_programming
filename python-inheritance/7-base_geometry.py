a149437981aced85f7bca499703ba8b76a3dcd56
root <tayler.coon@gmail.com>
2023-05-29 12:33:44 -0700

    task 7 setup

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' This module creates a class BaseGeometry '''
+
+
+class BaseGeometry:
+    ''' This class defines a BaseGeometry '''
+    def area(self):
+        ''' This method raises an exception '''
+        raise Exception('area() is not implemented')
