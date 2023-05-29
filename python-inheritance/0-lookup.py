70c43cc51fe4bf17622b6db13f0a912c0a544f5a
root <tayler.coon@gmail.com>
2023-05-29 10:30:51 -0700

    updated pycodestyling

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" function that returns the list of available attributes and methods of an object """
+""" function that returns the list of available attributes and methods
+of an object """
 
 
 def lookup(obj):
-    """ function that returns the list of available attributes and methods of an object """
+    """ function that returns the list of available attributes and methods
+    of an object """
     return dir(obj)
