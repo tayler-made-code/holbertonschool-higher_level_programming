ce664d11d11ae4da5767dc855e2e85a0b676bcf7
root <tayler.coon@gmail.com>
2023-05-29 14:08:35 -0700

    task 9 functional

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -11,3 +11,15 @@ class Rectangle(BaseGeometry):
         self.integer_validator('height', height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        ''' This method returns the area of a Rectangle '''
+        return self.__width * self.__height
+
+    def print(self):
+        ''' This method prints the Rectangle's dimensions '''
+        print('[Rectangle] {}/{}'.format(self.__width, self.__height))
+
+    def __str__(self):
+        ''' This method returns a string representation of a Rectangle '''
+        return '[Rectangle] {}/{}'.format(self.__width, self.__height)
