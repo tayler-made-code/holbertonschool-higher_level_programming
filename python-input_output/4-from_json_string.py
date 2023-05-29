54c498c3ff4aee13c6989591eacf0d8015d00e04
root <tayler.coon@gmail.com>
2023-05-29 15:06:12 -0700

    task 4 functional

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' Function that returns an opject (Python data structure) represented
+    by a JSON string '''
+import json
+
+
+def from_json_string(my_str):
+    ''' Function that returns an opject (Python data structure) represented
+        by a JSON string '''
+    return json.loads(my_str)
