0ce10de093ab0b73178070878ded50dd5b264604
root <tayler.coon@gmail.com>
2023-05-29 14:01:48 -0700

    updated edge cases

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,6 +11,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         ''' This method validates value '''
         if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(value))
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
