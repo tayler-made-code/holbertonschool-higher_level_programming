fe006fe68043760f6173de0ae373ea328ab8deed
root <tayler.coon@gmail.com>
2023-05-30 10:09:35 -0700

    task 7 functional

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 ''' adds all arguments to a Python list, and then save them to a file '''
 import sys
+import json
 
-if __name__ == "__main__":
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-    save_to_json_file = \
-        __import__('5-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_to_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        my_list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        my_list = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(my_list, "add_item.json")
+file = "add_item.json"
+try:
+    my_list = load_to_json_file(file)
+except FileNotFoundError:
+    my_list = []
+
+for args in sys.argv[1:]:
+    my_list.append(args)
+save_to_json_file(my_list, file)
