bb56ab85e3c69a2f187db3f81bee41fd8fd43bcb
root <tayler.coon@gmail.com>
2023-05-30 09:47:17 -0700

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+''' adds all arguments to a Python list, and then save them to a file '''
 from sys import argv
 ''' access command line arguments '''
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -9,7 +10,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 filename = "add_item.json"
 try:
     my_list = load_from_json_file(filename)
-except:
+else:
     my_list = []
 
 for i in range(1, len(argv)):
