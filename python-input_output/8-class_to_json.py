7fd4daf99a4578f2ed6d528628684aa6b6975eb0
root <tayler.coon@gmail.com>
2023-05-30 10:14:55 -0700

    task 8 functional

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' Function that returns the dictionary descriptions with a simple data
+    structure (list, dictionary, string, integer and boolean) for JSON
+    serialization of an object 
+    Prototype: def class_to_json(obj):
+    obj is an instance of a Class
+    All attributes of the obj Class are serializable: list, dictionary,
+    string, integer and boolean
+    You are not allowed to import any module
+    '''
+
+
+def class_to_json(obj):
+    ''' Function that returns the dictionary descriptions with a simple data
+        structure (list, dictionary, string, integer and boolean) for JSON
+        serialization of an object '''
+    return obj.__dict__
