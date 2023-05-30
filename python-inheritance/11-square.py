6d9e00de8efe5969a11a079c67bde6eb8b6ae4b7
root <tayler.coon@gmail.com>
2023-05-30 12:32:25 -0700

    updating project 10 & 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -7,14 +7,10 @@ class Square(Rectangle):
     ''' This class defines a Square '''
     def __init__(self, size):
         ''' This function initializes a Square '''
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def __str__(self):
         ''' This function returns a string representation of a Square '''
         return "[Square] {}/{}".format(self.__size, self.__size)
-
-    def area(self):
-        ''' This function returns the area of a Square '''
-        return self.__size ** 2
