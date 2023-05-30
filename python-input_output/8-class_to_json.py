b2023622885bc9eccad30e260a6e1f2b152f21e1
root <tayler.coon@gmail.com>
2023-05-30 10:16:06 -0700

    updated pycodestyling

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' Function that returns the dictionary descriptions with a simple data
     structure (list, dictionary, string, integer and boolean) for JSON
-    serialization of an object 
+    serialization of an object
     Prototype: def class_to_json(obj):
     obj is an instance of a Class
     All attributes of the obj Class are serializable: list, dictionary,
