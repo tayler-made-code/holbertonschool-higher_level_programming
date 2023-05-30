401290206a64086537bc9a1d9c976fc2eab2f027
root <tayler.coon@gmail.com>
2023-05-30 09:53:51 -0700

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 ''' adds all arguments to a Python list, and then save them to a file '''
-from sys import argv
-''' access command line arguments '''
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-''' creates an Object from a “JSON file” '''
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-''' writes an Object to a text file, using a JSON representation '''
+import sys
 
-filename = "add_item.json"
-try:
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
+if __name__ == "__main__":
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = \
+        __import__('5-save_to_json_file').save_to_json_file
 
-for i in range(1, len(argv)):
-    my_list.append(sys.argv[i])
-save_to_json_file(my_list, filename)
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(my_list, "add_item.json")
