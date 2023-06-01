3d1791c1057e287ee67d382e87f2152788ffaee3
root <tayler.coon@gmail.com>
2023-06-01 11:01:16 -0700

    task 1 functional, setting up tests

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+''' Base class for all other classes '''
 
+
+class Base:
+    ''' Base class for all other classes '''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        ''' Construction Junction, What's your function! '''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
