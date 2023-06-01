5301ef66f8f34df861ac0e8b9cdfdeb20611b9a5
root <tayler.coon@gmail.com>
2023-06-01 11:30:33 -0700

    making a rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 ''' this module creates a class Rectangle that inherits from base '''
+Base = __import__('base').Base
 
 
-
+class Rectangle(Base):
+    ''' This class defines a Rectangle '''
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
