cb9a360ca76466698d66baf041209acb92eb0271
root <tayler.coon@gmail.com>
2023-06-02 09:15:23 -0700

    setup create function

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -43,3 +43,13 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        ''' returns an instance with all attributes already set '''
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        elif cls.__name__ == "Square": 
+            new = cls(1)
+        new.update(**dictionary)
+        return new
