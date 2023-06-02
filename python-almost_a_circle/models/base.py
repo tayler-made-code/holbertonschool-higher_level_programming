e78b9aaf571510d4d7bbe7b0a1a24ca4ebed3e57
root <tayler.coon@gmail.com>
2023-06-02 08:51:13 -0700

    added to_json_string function to base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -13,3 +13,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        ''' returns JSON srting representation of list_dictionaries '''
+        import json
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
