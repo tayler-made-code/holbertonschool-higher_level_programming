7e4547094b39c31b265c0bffc64fbeb86aaa6d6a
root <tayler.coon@gmail.com>
2023-06-02 08:44:50 -0700

    added dictonary function to square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -52,3 +52,12 @@ class Square(Rectangle):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        ''' returns the dictionary representation of a Square '''
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+            }
