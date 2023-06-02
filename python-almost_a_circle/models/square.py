d5b15a500599f0146a6d4235bc04da40383621b1
root <tayler.coon@gmail.com>
2023-06-02 08:30:12 -0700

    added update for square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -25,7 +25,30 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        ''' size setter '''
+        ''' Would you like to supersize that? '''
         self.integer_validator("width", value)
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        ''' updates attributes of Square '''
+        if args:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+        elif args is None or len(args) == 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
