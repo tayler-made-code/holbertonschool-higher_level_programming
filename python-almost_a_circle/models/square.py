8b9d13fb8b46ab0c2197fe5ff995571cdab4c573
root <tayler.coon@gmail.com>
2023-06-02 07:44:25 -0700

    checking edge case

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,11 +9,11 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         ''' Construction Juntction Square's my function! '''
         self.integer_validator("size", size)
-        self.__size = width = height = size
-        super().__init__(width, height, x, y, id)
+        self.size = size
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         ''' returns string representation of Square '''
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width
+            self.id, self.x, self.y, self.size
             )
