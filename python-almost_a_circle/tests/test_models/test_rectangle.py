875e5308c3522ddf59bca1f6ca98e7ed78af1f96
root <tayler.coon@gmail.com>
2023-06-05 12:13:52 -0700

    better tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,13 +9,11 @@ class TestTangles(unittest.TestCase):
     ''' Unittest for class Rectangle '''
 
     def test_simple_tangle(self):
-        ''' Test simple rectangle '''
         testangle = Rectangle(13, 37)
         self.assertEqual(testangle.width, 13)
         self.assertEqual(testangle.height, 37)
     
     def test_tangle_with_args(self):
-        ''' Test rectangle with args '''
         testangle = Rectangle(1, 3, 3, 7, 89)
         self.assertEqual(testangle.width, 1)
         self.assertEqual(testangle.height, 3)
@@ -24,52 +22,62 @@ class TestTangles(unittest.TestCase):
         self.assertEqual(testangle.id, 89)
 
     def test_tangle_string_height(self):
-        ''' Test rectangle with string '''
         with self.assertRaises(TypeError):
             testangle = Rectangle("1", 3, 3, 7, 89)
     
     def test_tangle_string_width(self):
-        ''' Test rectangle with string '''
         with self.assertRaises(TypeError):
             testangle = Rectangle(1, "3", 3, 7, 89)
     
     def test_tangle_string_x(self):
-        ''' Test rectangle with string '''
         with self.assertRaises(TypeError):
             testangle = Rectangle(1, 3, "3", 7, 89)
     
     def test_tangle_string_y(self):
-        ''' Test rectangle with string '''
         with self.assertRaises(TypeError):
             testangle = Rectangle(1, 3, 3, "7", 89)
     
     def test_tangle_negative_height(self):
-        ''' Test rectangle with negative '''
         with self.assertRaises(ValueError):
-            testangle = Rectangle(-13, 37)
+            testangle = Rectangle(13, -37)
     
     def test_tangle_negative_width(self):
-        ''' Test rectangle with negative '''
         with self.assertRaises(ValueError):
             testangle = Rectangle(-13, 37)
     
     def test_tangle_negative_x(self):
-        ''' Test rectangle with negative '''
         with self.assertRaises(ValueError):
             testangle = Rectangle(1, 3, -3, 7, 89)
     
     def test_tangle_negative_y(self):
-        ''' Test rectangle with negative '''
         with self.assertRaises(ValueError):
             testangle = Rectangle(1, 3, 3, -7, 89)
     
     def test_tangle_zero_height(self):
-        ''' Test rectangle with zero '''
         with self.assertRaises(ValueError):
             testangle = Rectangle(0, 37)
     
     def test_tangle_zero_width(self):
-        ''' Test rectangle with zero '''
         with self.assertRaises(ValueError):
             testangle = Rectangle(13, 0)
 
+    def test_tangle_str_function(self):
+        testangle = Rectangle(3, 7, 1, 3, 89)
+        self.assertEqual(testangle.__str__(), "[Rectangle] (89) 1/3 - 3/7")
+    
+    def test_tangle_to_dictionary(self):
+        testangle = Rectangle(1, 3, 3, 7, 89)
+        self.assertEqual(testangle.to_dictionary(), {'x': 3, 'y': 7, 'id': 89, 'height': 3, 'width': 1})
+    
+    def test_tangle_update(self):
+        testangle = Rectangle(1, 3, 3, 7, 89)
+        testangle.update(1, 2, 3, 4, 5)
+        self.assertEqual(testangle.__str__(), "[Rectangle] (1) 4/5 - 2/3")
+    
+    def test_tangle_update_kwargs(self):
+        testangle = Rectangle(1, 2, 3, 4, 5)
+        testangle.update(height=1, width=3, x=3, y=7, id=89)
+    
+    def test_tangle_area(self):
+        testangle = Rectangle(3, 2)
+        self.assertEqual(testangle.area(), 6)
