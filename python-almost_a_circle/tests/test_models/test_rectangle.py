6e6900ba4268f0b344e44226ff6629405f0aca97
root <tayler.coon@gmail.com>
2023-06-05 08:23:40 -0700

    added more testangles

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -14,3 +14,105 @@ class TestRectangleArea(unittest.TestCase):
         testangle = Rectangle(8, 7, 0, 0, 12)
         self.assertEqual(testangle.area(), 56)
 
+class TestWidthSetter(unittest.TestCase):
+    ''' Unittest for width setter '''
+
+    def test_width_setter(self):
+        testangle = Rectangle(3, 2)
+        testangle.width = 10
+        self.assertEqual(testangle.width, 10)
+    
+    def test_width_setter_with_args(self):
+        testangle = Rectangle(8, 7, 0, 0, 12)
+        testangle.width = 10
+        self.assertEqual(testangle.width, 10)
+    
+    def test_width_setter_with_string(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = "Thirteen"
+    
+    def test_width_setter_with_negative(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(ValueError):
+            testangle.width = -13
+    
+    def test_width_setter_with_zero(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(ValueError):
+            testangle.width = 0
+    
+    def test_width_setter_with_float(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = 13.5
+    
+    def test_width_setter_with_tuple(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = (13, 37)
+    
+    def test_width_setter_with_dict(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = {13: 37}
+    
+    def test_width_setter_with_dict(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = [13, 37]
+    
+    def test_width_setter_with_set(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = {13, 37}
+    
+    def test_width_setter_with_bool(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = True
+    
+    def test_width_setter_with_None(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = None
+    
+    def test_width_setter_with_inf(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = float('inf')
+
+    def test_width_setter_with_nan(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = float('nan')
+    
+    def test_width_setter_with_negative_inf(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = float('-inf')
+    
+    def test_width_setter_with_negative_inf(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = float('-inf')
+    
+    def test_width_setter_with_negative_nan(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = float('-nan')
+    
+    def test_width_setter_with_complex(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = complex(13, 37)
+    
+    def test_width_setter_with_complex(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width = complex(13, 37)
+    
+    def test_width_setter_with_zero_args(self):
+        testangle = Rectangle(3, 2)
+        with self.assertRaises(TypeError):
+            testangle.width()
