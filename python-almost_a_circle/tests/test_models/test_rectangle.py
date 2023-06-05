296672927d6d207f09f0048a3a19dda44d025165
root <tayler.coon@gmail.com>
2023-06-05 10:19:15 -0700

    fixed tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -42,77 +42,39 @@ class TestWidthSetter(unittest.TestCase):
         with self.assertRaises(ValueError):
             testangle.width = 0
     
-    def test_width_setter_with_float(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = 13.5
-    
-    def test_width_setter_with_tuple(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = (13, 37)
-    
-    def test_width_setter_with_dict(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = {13: 37}
-    
-    def test_width_setter_with_dict(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = [13, 37]
-    
-    def test_width_setter_with_set(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = {13, 37}
-    
-    def test_width_setter_with_bool(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = True
-    
-    def test_width_setter_with_None(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = None
-    
-    def test_width_setter_with_inf(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = float('inf')
-
-    def test_width_setter_with_nan(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = float('nan')
-    
-    def test_width_setter_with_negative_inf(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = float('-inf')
-    
-    def test_width_setter_with_negative_inf(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = float('-inf')
-    
-    def test_width_setter_with_negative_nan(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = float('-nan')
-    
-    def test_width_setter_with_complex(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = complex(13, 37)
-    
-    def test_width_setter_with_complex(self):
-        testangle = Rectangle(3, 2)
-        with self.assertRaises(TypeError):
-            testangle.width = complex(13, 37)
-    
-    def test_width_setter_with_zero_args(self):
         testangle = Rectangle(3, 2)
         with self.assertRaises(TypeError):
             testangle.width()
+
+class TestRectangleUpdate(unittest.TestCase):
+    ''' Unittest for update '''
+
+    def test_update(self):
+        testangle = Rectangle(1, 3, 3, 7)
+        testangle.update(89, 3, 7, 1, 3)
+        self.assertEqual(testangle.__str__(), "[Rectangle] (89) 1/3 - 3/7")
+
+    def test_update_with_kwargs(self):
+        testangle = Rectangle(10, 10, 10, 10, 10)
+        testangle.update(id=89, width=3, height=7, x=1, y=3)
+        self.assertEqual(testangle.__str__(), "[Rectangle] (89) 1/3 - 3/7")
+
+    def test_update_with_args_and_kwargs(self):
+        testangle = Rectangle(1, 3, 3, 7)
+        testangle.update(89, 1, 2, 3, 4, id=90, width=2, height=3, x=4, y=5)
+        self.assertEqual(testangle.__str__(), "[Rectangle] (89) 3/4 - 1/2")
+
+    def test_update_with_no_string(self):
+        testangle = Rectangle(1, 3, 3, 7)
+        with self.assertRaises(TypeError):
+            testangle.update("Eighty-Nine", "Three", "Seven", "One", "Three")
+
+    def test_update_with_negative(self):
+        testangle = Rectangle(1, 3, 3, 7)
+        with self.assertRaises(ValueError):
+            testangle.update(-89, -3, -7, -1, -3)
+
+    def test_update_with_zero(self):
+        testangle = Rectangle(1, 3, 3, 7)
+        with self.assertRaises(ValueError):
+            testangle.update(0, 0, 0, 0, 0)
