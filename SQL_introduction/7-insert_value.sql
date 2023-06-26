5d6084c8ee51188c2cb23b33fbc9546a927e54ab
root <tayler.coon@gmail.com>
2023-06-26 16:10:29 -0700

    fixing typos

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- script that inserts a new row into a table in the MySQL server
-INSERT INTO first_table (id, name) VAULES(89, "Best School")
+INSERT INTO first_table(id, name) VALUES(89, "Best School")
