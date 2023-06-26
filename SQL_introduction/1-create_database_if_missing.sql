2a6f64ecb33e3e6051736c99fec13f6e8341f37f
root <tayler.coon@gmail.com>
2023-06-26 15:39:05 -0700

    should NOT fail

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- script that creates a database in the MySQL server
--- if the database already exists, the script should fail
+-- if the database already exists, the script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- script that deletes a database in the MySQL server
+-- if the database does not exist, the script should not fail
+DROP DATABASE IF EXISTS hbtn_0c_0
