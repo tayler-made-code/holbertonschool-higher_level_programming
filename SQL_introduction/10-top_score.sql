d9a763d18e4178d7440b09ee2227be72a84090c3
root <tayler.coon@gmail.com>
2023-06-27 10:20:53 -0700

    task 10

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- script that lists all records of the second_table of the hbtn_0c_0 database in the MySQL server
 SELECT score, name
-FROM hbtn_0c_0.second_table
+FROM second_table
 ORDER BY score DESC;
