eafcf94511acd0905feda581743beccb14803707
root <tayler.coon@gmail.com>
2023-06-27 16:01:24 -0700

    task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- script that creates a server user with the name user_0d_1 and the password user_0d_1_pwd
-CREATE USER IF NOT EXISTS "user_0d_1"@"localhost";
-SET PASSWORD FOR "user_0d_1"@"localhost" = "user_0d_1_pwd";
-GRANT ALL PRIVILEGES ON *.* TO "user_0d_1"@"localhost";
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
