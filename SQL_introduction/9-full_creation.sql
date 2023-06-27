a974f3604d326c2c2f5c19616f735f300c9c4a3e
root <tayler.coon@gmail.com>
2023-06-26 19:30:12 -0700

    fixed syntax

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,8 +4,10 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256)
     score INT
 );
+
 INSERT INTO second_table
-VALUES (1, 'John', 10),
-       (2, 'Alex', 3),
-       (3, 'Bob', 14);
-       (4, 'George', 8);
+VALUES 
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
