5968fd0949f7208b61a0a10c465a8c1f8f92b33a
root <tayler.coon@gmail.com>
2023-06-29 09:42:16 -0700

    task 5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,6 @@
 -- script that creates a table unique_id on your MySQL server
+-- script that creates a table unique_id on your MySQL server
 CREATE TABLE if NOT EXISTS unique_id (
-    id INT NOT NULL UNIQUE,
+    id INT NOT NULL UNIQUE SET DEFAULT 1,
     name VARCHAR(256)
 )
