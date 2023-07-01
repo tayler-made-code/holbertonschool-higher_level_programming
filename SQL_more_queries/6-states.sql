1202ac7380d0aecbe51cca6762f239484328dd30
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-01 12:20:18 -0700

    task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- script that creates a database hbtn_0d_usa and the table states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256) NOT NULL,
