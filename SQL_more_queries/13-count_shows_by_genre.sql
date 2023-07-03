195a16b1eca87774c3bbaefc04bc798d1318c0cc
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-03 13:29:30 -0700

    task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- import database dump from hbtn_0d_tvshows to the download server
 -- script that lists all genres from hbtn_0d_tvshows and display the number of shows linked to each
 SELECT DISTINCT tv_genres.name AS genre,
-COUNT(tv_shows.id) AS number_of_shows
+COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
