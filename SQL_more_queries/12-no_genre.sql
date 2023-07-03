f9e1c5285acad34f3f204a3e0a234d59d9a4e8a6
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-03 12:53:56 -0700

    task 12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- import database dump from hbtn_0d_tvshows to the download server
 -- script that lists all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT DISTINCT tv_shows.title, tv_shows_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genresn ON tv_shows.id = tv_show_genres.show_id
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name IS NULL
 ORDER BY tv_shows.title ASC; tv_show_genres.genre_id ASC;
