6694d946bf2348e0103e7f944f5d723cc2bffb12
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-04 11:30:24 -0700

    removed white space

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -12,7 +12,7 @@ Base = declarative_base()
 
 class State(Base):
     """ State class """
-    
+ 
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
