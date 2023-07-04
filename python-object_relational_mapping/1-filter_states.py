88c3ee34a93bc463b5cc0d0fb062b62078755c62
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-04 11:11:43 -0700

    task 0-3

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Write a script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa """
+
+import MySQLdb
+import sys
+
+
+def list_N_states():
+    """ List the states that start with 'N '"""
+    
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    list_N_states()
---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Write a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument. """
+
+import MySQLdb
+import sys
+
+
+def list_states_by_name():
+    """ List the state specified """
+    
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY\
+                 '{}' ORDER BY id".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    list_states_by_name()
