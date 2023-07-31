8369e62b1122b866ba52943ce488b710830c994d
root <tayler.coon@gmail.com>
2023-07-31 12:02:55 -0700

    fixing semistandard

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 function wellActually (n) {
-    console.log(n);
+  console.log(n);
+}
+if (isNaN(process.argv[2]) || process.argv[2] <= 1) {
+  console.log('1');
+} else {
+  let input = parseInt(process.argv[2]);
+  for (let i = input - 1; i >= 1; i--) {
+    input *= i;
   }
-
-  if (isNaN(process.argv[2]) || process.argv[2] === 0 || process.argv[2] === 1) {
-    console.log('1');
-  } else {
-    let n = parseInt(process.argv[2]);
-    for (let i = n - 1; i >= 1; i--) {
-        n *= i;
-        }
-    wellActually(n);
-    }
-
+  wellActually(input);
+}
