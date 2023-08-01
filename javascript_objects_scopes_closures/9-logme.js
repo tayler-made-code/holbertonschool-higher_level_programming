cd633c4ce218258335f2a5ec637da7324ab48ba8
root <tayler.coon@gmail.com>
2023-08-01 14:54:59 -0700

    task 9

---
+++ javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.logMe = function (item) {
+  if (exports.logMe.count === undefined) {
+      exports.logMe.count = 0;
+  }
+  console.log(exports.logMe.count + ': ' + item);
+  exports.logMe.count++;
+  }
