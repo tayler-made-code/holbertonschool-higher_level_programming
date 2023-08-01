c3cf60696e8b6f7c289d5daf662a07221f0bad89
root <tayler.coon@gmail.com>
2023-07-31 18:18:15 -0700

    task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,9 +5,9 @@ if (process.argv.length <= 3) {
 } else {
   const totalArgs = process.argv.length;
   let currentHighest = parseInt(process.argv[2]);
-  let secondHighest = currentHighest;
+  let secondHighest = parseInt(process.argv[3]);
 
-  for (let iterate = 2; iterate < totalArgs; iterate++) {
+  for (let iterate = 3; iterate < totalArgs; iterate++) {
     const compare = parseInt(process.argv[iterate]);
     if (currentHighest < compare) {
       secondHighest = currentHighest;
