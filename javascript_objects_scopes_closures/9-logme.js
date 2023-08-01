b66e78726f8c944646ee6a800633e2cc9012d880
root <tayler.coon@gmail.com>
2023-08-01 14:57:33 -0700

    task 9 semistandard compliant

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -2,8 +2,8 @@
 
 exports.logMe = function (item) {
   if (exports.logMe.count === undefined) {
-      exports.logMe.count = 0;
+    exports.logMe.count = 0;
   }
   console.log(exports.logMe.count + ': ' + item);
   exports.logMe.count++;
-  }
+};
