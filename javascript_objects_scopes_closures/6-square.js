2f5c32726bee6e9cae448e5c34792f2c31ccc848
root <tayler.coon@gmail.com>
2023-08-01 13:52:21 -0700

    task 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const definedSquare = require('./5-square');
+
+class Square extends definedSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
