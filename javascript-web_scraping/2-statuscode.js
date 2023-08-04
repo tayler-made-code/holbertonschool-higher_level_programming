018d641880226c2a9a4483e84ad4c66e01c565c4
root <tayler.coon@gmail.com>
2023-08-04 19:10:19 +0000

    task 1

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const textToAdd = process.argv[3];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+
+    if (textToAdd) {
+        fs.appendFile(filePath, textToAdd, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+  }
+});
