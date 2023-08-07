70769d17459865d1294bfc1065f73716e0446ce8
root <tayler.coon@gmail.com>
2023-08-07 13:46:58 +0000

    task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,21 +1,12 @@
 #!/usr/bin/node
 
+// To include the File System module, use the require() method
 const fs = require('fs');
 const filePath = process.argv[2];
 const textToAdd = process.argv[3];
 
-fs.readFile(filePath, 'utf8', (err, data) => {
+fs.appendFile(filePath, textToAdd, (err) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
-
-    if (textToAdd) {
-      fs.appendFile(filePath, textToAdd, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-    }
   }
 });
