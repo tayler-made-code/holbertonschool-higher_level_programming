62e5d6175abf1e49a408aac763a1d14579b99966
root <tayler.coon@gmail.com>
2023-08-07 17:46:06 +0000

    task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    if (res.statusCode === 200) {
+      fs.writeFile(filePath, body, 'utf-8', (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+  }
+});
